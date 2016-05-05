498e9dff66eafa4704a6976f65d876eb17001f14
Khurshid Alam <contactkhalam@gmail.com>
2016-05-05 16:50:19 +0100

    update to the buildscript file

--- buildtest.sh
+++ buildtest.sh
@@ -2,11 +2,11 @@ echo "Hi Afsha "
 giturl="$1"
 gituser="$2"
 gitpassword="$3"
-workingdirectory="$4"
+
 echo "passed GIT URL is $gitURL"
 echo "passed GIT User is $gituser"
 echo "passed GIT password is $gitpassword"
-echo "working directory is $workingdirectory"
+
 
 #git clone $giturl
 #cd vendmanagementsystem
