c58ef22d4de33f8b4578fad15348c587c854a68a
Khurshid Alam <contactkhalam@gmail.com>
2016-05-05 16:35:17 +0100

    test scripts to be added for the first time

---
+++ buildtest.sh
@@ -0,0 +1,13 @@
+echo "Hi Afsha "
+giturl="$1"
+gituser="$2"
+gitpassword="$3"
+workingdirectory="$4"
+echo "passed GIT URL is $gitURL"
+echo "passed GIT User is $gituser"
+echo "passed GIT password is $gitpassword"
+echo "working directory is $workingdirectory"
+
+#git clone $giturl
+#cd vendmanagementsystem
+#mvn clean install
