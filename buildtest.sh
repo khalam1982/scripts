4fe9cdd093148dbcb2133c520b5c961561bd6659
Khurshid Alam <contactkhalam@gmail.com>
2016-05-05 17:50:42 +0100

    update to the buildscript file

--- buildtest.sh
+++ buildtest.sh
@@ -2,12 +2,17 @@ echo "Hi Afsha "
 giturl="$1"
 gituser="$2"
 gitpassword="$3"
-
+directory="game-of-life"
 echo "passed GIT URL is $gitURL"
 echo "passed GIT User is $gituser"
 echo "passed GIT password is $gitpassword"
 
+if [ -d "$directory" ]; then
+echo "game-of-life directory exists"
+fi
 
-git clone $giturl
+#git clone $giturl
+
+
 #cd vendmanagementsystem
 #mvn clean install
