cb2e151a93dd897bdd8b9d7eee33ed91f8a6dee0
Khurshid Alam <contactkhalam@gmail.com>
2016-05-05 18:13:40 +0100

    update to the buildscript file

--- buildtest.sh
+++ buildtest.sh
@@ -1,14 +1,11 @@
-echo "Hi Afsha "
-giturl="$1"
-directory="$2"
-echo "passed GIT URL is $giturl"
+
+
+directory="$1"
+
 echo "passed GIT directory is $directory"
 
-if [ -d "$directory" ]; then
-echo "game-of-life directory exists"
-fi
 
-git clone $giturl
+
 if [ -d "$directory" ]; then
 echo "game-of-life directory exists"
 fi
